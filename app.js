9dfc23dfbfb97f4089e5c1207530929f015b6cf3
James <tytechortz@gmail.com>
2018-09-17 18:56:46 -0600

    data types

---
+++ app.js
@@ -0,0 +1,23 @@
+//1. Data types
+//a-boolean, either off or on, true or false. on == true || false
+//b-string "someone@email.com"
+/*c-object const spaceship = {
+    hullType: 'something',
+    weapon: 'laser blasters',
+    defense: 'tractor beam',
+    motor: 'warp drive',
+}
+//d-array [jim, john, mary]
+//e-object const students = {
+    jim: denver,
+    john: philly,
+    mary: rancho cucamonga
+}
+//f-array of objects
+const students = [
+    {name: jim, location: denver}
+    {name: john, location: philly}
+    {name: mary, location: rancho cucamonga}
+]
+*/
+
